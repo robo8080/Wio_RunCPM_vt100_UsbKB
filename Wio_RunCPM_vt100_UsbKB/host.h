5f8642c1d6f9cd505a2030d21c4527f496944390
DEKO <deko@ht-deko.com>
2021-01-29 23:05:11 +0900

    下位バイトを機能No、上位バイトをパラメータにした

--- Wio_RunCPM_vt100_UsbKB/host.h
+++ Wio_RunCPM_vt100_UsbKB/host.h
@@ -2,13 +2,17 @@
 #define HOST_H
 
 uint16 hostbdos(uint16 dmaaddr) {
-	switch(dmaaddr) {
-  case 1:
-    return(F_CPU / 1000000L); // CPU Speed
-    break;
-	default:
-    return(0x0000);
-	}
+
+  uint8 func  = LOW_REGISTER(dmaaddr);
+  uint8 param = HIGH_REGISTER(dmaaddr);
+
+  switch (func) {
+    case 1:
+      return (F_CPU / 1000000L); // CPU Speed
+      break;
+    default:
+      return (0x0000);
+  }
 }
 
 #endif
