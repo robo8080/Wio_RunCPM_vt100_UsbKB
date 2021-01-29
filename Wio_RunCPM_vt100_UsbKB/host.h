b08dd16455dcda3e97fac61aa2309477df79b783
DEKO <deko@ht-deko.com>
2021-01-29 22:23:01 +0900

    hostbdos(1) を実装
    1: CPU 速度 (MHz) を返す

--- Wio_RunCPM_vt100_UsbKB/host.h
+++ Wio_RunCPM_vt100_UsbKB/host.h
@@ -1,8 +1,14 @@
 #ifndef HOST_H
 #define HOST_H
 
-uint8 hostbdos(uint16 dmaaddr) {
-	return(0x00);
+uint16 hostbdos(uint16 dmaaddr) {
+	switch(dmaaddr) {
+  case 1:
+    return(F_CPU / 1000000L); // CPU Speed
+    break;
+	default:
+    return(0x0000);
+	}
 }
 
 #endif
